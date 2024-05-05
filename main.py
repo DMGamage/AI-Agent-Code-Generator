057f581d2e515b89c2aa99400262db022c2dad37
DMGamage <gamagemadhushan@gmail.com>
2024-05-05 11:44:34 +0530

    Code generator using second llm complete

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from llama_parse import LlamaParse
 from llama_index.core import VectorStoreIndex,SimpleDirectoryReader,PromptTemplate
 from llama_index.core.embeddings import resolve_embed_model
 from dotenv import load_dotenv
+from llama_index.core.tools import QueryEngineTool,ToolMetadata
+from llama_index.core.agent import ReActAgent
+from code_reader import code_reader # type: ignore
+from pydantic import BaseModel
+from llama_index.core.output_parsers import PydanticOutputParser
+from llama_index.core.query_pipeline import QueryPipeline
+
+
+context = """Purpose: The primary role of this agent is to assist users by analyzing code. It should
+            be able to generate code and answer questions about code provided. """
+
+code_parser_template = """Parse the response from a previous LLM into a description and a string of valid code, 
+                            also come up with a valid filename this could be saved as that doesnt contain special characters. 
+                            Here is the response: {response}. You should parse this in the following JSON Format: """
 
 load_dotenv()
 
@@ -19,17 +33,38 @@ embed_model = resolve_embed_model("local:BAAI/bge-m3")
 vector_index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
 query_engine = vector_index.as_query_engine(llm=llm)
 
-result = query_engine.query("What are some of the routes in the api?")
-print(result)
+tools = [
+    QueryEngineTool(
+        query_engine=query_engine,
+        metadata=ToolMetadata(
+            name="api_documentation",
+            description="this gives documentation about code for an API.Use this reading docs for the API"
+        )
+    ),
+    code_reader,
+]
 
-# output = 
-# The API supports several routes for performing various operations. These include:
+code_llm = Ollama(model="codellama")
+agent = ReActAgent.from_tools(tools,llm=code_llm,verbose=True,context=context)
 
-# 1. `/items` (GET): Retrieve all items
-# 2. `/items` (POST): Create a new item
-# 3. `/items/<item_id>` (GET): Retrieve a single item by its ID
-# 4. `/items/<item_id>` (PUT): Update an existing item by its ID
-# 5. `/items/<item_id>` (DELETE): Delete an item by its ID
+class CodeOutput(BaseModel):
+    code: str
+    description: str
+    filename: str
+
+
+parser = PydanticOutputParser(CodeOutput)
+json_prompt_str = parser.format(code_parser_template)
+json_prompt_tmpl = PromptTemplate(json_prompt_str)
+output_pipeline = QueryPipeline(chain=[json_prompt_tmpl, llm])
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+
+    result = agent.query(prompt)
+    print(result)
+
+
+
 
 
 
